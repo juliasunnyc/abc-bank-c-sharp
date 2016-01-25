c27d42eb5aea7983c856f2a1b9bb29106dd3b590
juliasunnyc <crazygemin568@gmail.com>
2016-01-25 12:15:49 -0500

    Update Transaction.cs
    restructured transaction object

--- AbcBank/Transaction.cs
+++ AbcBank/Transaction.cs
@@ -1,4 +1,4 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +6,67 @@ using System.Threading.Tasks;
 
 namespace AbcBank
 {
-    public class Transaction
+    public abstract class transaction
     {
-        public readonly double amount;
+        public virtual double amount { get; set; }
+        public virtual string transactionType { get; set; }
+        public abstract DateTime transactionDate { get; set; }
+      
+    }
+  
+    public class Transaction :transaction
+    {
+        public  double amt;
+        private string transType;
+        private DateTime transDate;
+        public const string WITHDRAW = "withdraw";
+        public const string DEPOSITE = "deposite";
+        public const string  TRANSFER =  "transfer" ;
 
-        private DateTime transactionDate;
-
-        public Transaction(double amount)
+        public override double amount
         {
-            this.amount = amount;
+            get { 
+                  return amt;
+                 }
+            set {
+                amount = value;
+              }
+
+        }
+
+        public override string transactionType
+        {
+            get
+            {
+                return transType;
+            }
+            set
+            {
+                transType = value;
+            }
+
+        }
+
+        public override  DateTime transactionDate
+        {
+            get
+            {
+                return transDate;
+            }
+            set
+            {
+                transDate = value;
+            }
+
+        }
+
+
+        public Transaction( double amounts, string type)
+        {
+
+            this.amount = amounts;
             this.transactionDate = DateProvider.getInstance().now();
+            this.transType = type;
         }
 
     }
